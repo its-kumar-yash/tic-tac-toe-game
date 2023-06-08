f36c50cf2ff0c220cc31770d2e538c1137b26cf9
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-08 18:31:41 +0530

    storing and managing submitted data

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,17 @@
+let editedPlayer = 0;
+
+const players = [
+    {
+        name: '',
+        symbol: 'X' 
+    },
+    {
+        name: '',
+        symbol: 'O'
+    }
+];
+
+
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
@@ -8,7 +22,7 @@ const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
-editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  const selectedPlayerId = +event.target.dataset.playerid; //+'1' -> 1
+  editedPlayer = selectedPlayerId;
   playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -8,7 +10,7 @@ function closePlayerConfig() {
   backdropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
   errorsOutputElement.textContent = "";
-
+  formElement.firstElementChild.lastElementChild.value = '';
 }
 
 function savePlayerConfig(event) {
@@ -22,4 +24,14 @@ function savePlayerConfig(event) {
     errorsOutputElement.style.color = "red";
     return;
   }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;//h3 element
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+  
+  closePlayerConfig();
 }
