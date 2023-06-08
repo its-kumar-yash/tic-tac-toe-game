5ca73c432cd26afd11da1d4805d7cc1fbb006108
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-08 20:59:43 +0530

    added win/draw logic

--- scripts/game.js
+++ scripts/game.js
@@ -24,18 +24,67 @@ function selectGameField(event) {
   const selectedColumn = selectedField.dataset.col - 1;
   const selectedRow = selectedField.dataset.row - 1;
 
-  if(gameData[selectedRow][selectedColumn] > 0){
+  if (gameData[selectedRow][selectedColumn] > 0) {
     alert("Please select an empty field!");
     return;
   }
 
-
   event.target.textContent = players[activePlayer].symbol;
   event.target.classList.add("disabled");
 
-
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  console.log(gameData);
-
+  // console.log(gameData);
+  const winnerId = checkForGameOver(gameData);
+  console.log(winnerId);
+  
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  //Checking Rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  //Checking Rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  //Checking Diagonal for equality
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if(currentRound === 9){
+    return -1;
+  }
+
+  //No Winner
+  return 0;
 }
